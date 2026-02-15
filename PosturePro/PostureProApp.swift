0fc5cf51b567c5a0fa5791e3081a35c2b352fa5c
Momo <mohamedbakloul98@gmail.com>
2026-02-15 18:40:25 +0100

    Initial Commit

---
+++ PosturePro/PostureProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureProApp.swift
+//  PosturePro
+//
+//  Created by Mohamed Bakloul on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PostureProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
